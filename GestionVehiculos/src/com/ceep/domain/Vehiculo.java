0654fcf5a1e21ea10d1e1f2a0a7ba8c888a8dc5d
Rengoan <91058472+Rengoan@users.noreply.github.com>
2021-09-28 11:34:27 +0200

    Vehiculo2.0

--- GestionVehiculos/src/com/ceep/domain/Vehiculo.java
+++ GestionVehiculos/src/com/ceep/domain/Vehiculo.java
@@ -2,4 +2,86 @@ package com.ceep.domain;
 
 public class Vehiculo {
 
+    private String matricula, marca, modelo, color;
+    private Double tarifa;
+    private Boolean disponible;
+
+    //Getters and Setters
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Double getTarifa() {
+        return tarifa;
+    }
+
+    public void setTarifa(Double tarifa) {
+        this.tarifa = tarifa;
+    }
+
+    public Boolean getDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(Boolean disponible) {
+        this.disponible = disponible;
+    }
+    
+    //Constructores
+
+    public Vehiculo(String matricula, String marca, String modelo, String color, Double tarifa, Boolean disponible) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.tarifa = tarifa;
+        this.disponible = disponible;
+             
+    }
+
+    public Vehiculo() {
+    }
+    
+    //Metodos
+    
+    public void mostrar() {
+        System.out.println("Matricula: " + matricula);
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Color: " + color);
+        System.out.println("Tarifa: " + tarifa);
+        System.out.println("Disponible: " + disponible);
+
+    }
+    
+    
+
 }
--- GestionVehiculos/src/com/ceep/test/PruebaGestion.java
+++ GestionVehiculos/src/com/ceep/test/PruebaGestion.java
@@ -1,9 +1,13 @@
 package com.ceep.test;
 
+import com.ceep.domain.Vehiculo;
+
 public class PruebaGestion {
 
     public static void main(String[] args) {
 
+        Vehiculo v1= new Vehiculo("3522FZK","Ford","Focus","Azul",690.200,true);
+        v1.mostrar();
     }
 
 }
